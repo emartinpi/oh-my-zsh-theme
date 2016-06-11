2537d50311c47b4b677422e0182ef5c3677c1801
sedux <emartinpi@users.noreply.github.com>
2016-06-11 12:59:01 +0200

    Little scripts to help me in my daily life :)

---
+++ edu.zsh
@@ -0,0 +1,39 @@
+# You can put files here to add functionality separated per file, which
+# will be ignored by git.
+# Files on the custom/ directory will be automatically loaded by the init
+# script, in alphabetical order.
+
+# For example: add yourself some shortcuts to projects you often work on.
+#
+# brainstormr=~/Projects/development/planetargon/brainstormr
+# cd $brainstormr
+#
+
+#shopping=~/workspace/pruebas/cells-hackaton/cells-shoppin
+
+# set up a quick web server
+function server() {  
+  if [ $1 ]
+  then
+    local port="$1"
+  else
+    local port="8000"
+  fi
+  open "http://localhost:$port" && python -m SimpleHTTPServer "$port"
+}
+
+# Quickly creates a folder
+function take() {  
+    mkdir $1
+    cd $1
+}
+
+# Show&hide hidden files
+function hiddenfiles() {
+    if [ $1 ]; then
+      defaults write com.apple.finder AppleShowAllFiles $1
+      killAll Finder
+    else
+      echo "zsh: TRUE or FALSE attribute missing"
+    fi
+}
